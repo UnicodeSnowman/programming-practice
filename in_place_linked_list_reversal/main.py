29e10765470a2809b4e6e10c9d7b0c4b85f46977
unicodesnowman <utfsnowman@gmail.com>
2015-05-28 08:59:11 -0400

    Add instruction text

--- in_place_linked_list_reversal/main.py
+++ in_place_linked_list_reversal/main.py
@@ -1,3 +1,9 @@
+# Hooray! It's opposite day. Linked lists go the opposite way today.
+# Write a function for reversing a linked list ↴ . Do it in-place ↴ .
+# 
+# Your function will have one input: the head of the list.
+# 
+# Your function should return the new head of the list.
 
 class ListItem:
   def __init__(self, value, next):
@@ -9,10 +15,13 @@ def reverse(head):
 
   current = head
   previous = None
+  next = None
 
   while (current is not None):
     next = current.next
     current.next = previous
+
+    # previous (eventually) becomes our new head.
     previous = current
     current = next
 
