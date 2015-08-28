5627f4bd7fa01119be7758095f063f281368e2c5
unicodesnowman <utfsnowman@gmail.com>
2015-08-28 18:03:56 -0400

    merge_sorted_arrays

---
+++ merge_sorted_arrays/main.py
@@ -0,0 +1,15 @@
+# In order to win the prize for most cookies sold, my friend Alice and I are going to merge our Girl Scout Cookies orders and enter as one unit.
+# Each order is represented by an "order id" (an integer).
+# 
+# We have our lists of orders sorted numerically already, in arrays. Write a function to merge our arrays of orders into one sorted array.)
+
+# O(n) ?
+def merge_arrays(one, two):
+  both = []
+  return both
+
+one = [3, 4, 6, 10, 11, 15]
+two = [1, 5, 8, 12, 14, 19]
+
+print(merge_arrays(one, two))
+
