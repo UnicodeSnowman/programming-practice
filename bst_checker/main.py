20f9c2740222f795277be60d598d329c33e99228
unicodesnowman <utfsnowman@gmail.com>
2015-11-05 19:09:19 -0500

    trying some other ideas

--- bst_checker/main.py
+++ bst_checker/main.py
@@ -10,17 +10,55 @@ class BinaryTreeNode:
 
 def bst_checker(tree):
 
-  def is_valid(value, left, right, ancestors):
-    if not left:
-      return True
+  def is_valid_right(value, left, right, ancestors):
+    # is greater than all ancestors
+    for ancestor in ancestors:
+      if value < ancestor:
+        return False
 
-    ancestors[value] = True
+    ancestors.append(value)
+
+    if left is None:
+      left_result = False
+    else:
+      left_result = is_valid_right(left.value, left.left, left.right, ancestors)
+
+    if right is None:
+      right_result = False
+    else:
+      right_result = is_valid_right(right.value, right.left, right.right, ancestors)
+
+    return left_result and right_result
+
+  def is_valid_left(value, left, right, ancestors):
+    # is less than all ancestors
+    for ancestor in ancestors:
+      if value > ancestor:
+        return False
+
+    if left is None:
+      left_result = True
+    elif left.value > value:
+      return False
+    else:
+      left_result = is_valid_left(left.value, left.left, left.right, ancestors + [value])
+
+    if right is None:
+      right_result = True
+    elif right.value < value:
+      return False
+    else:
+      right_result = is_valid_left(right.value, right.left, right.right, ancestors)
+
+    return left_result and right_result
 
     return (
-        is_valid(left.value, left.left, left.right, ancestors) and
+        is_valid_left(left.value, left.left, left.right, ancestors) and
         is_valid(right.value, right.left, right.right, ancestors))
 
-  return is_valid(tree.value, tree.left, tree.right, { 50: True })
+  return (
+    is_valid_left(tree.value, tree.left, None, []) and True)
+    #is_valid_right(tree.value, None, tree.right, [tree.value]))
 
 a = BinaryTreeNode(2,
       BinaryTreeNode(1),
